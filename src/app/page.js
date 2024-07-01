37edab8b1c0c7f8168aef861686c119da6b18e4a
Saviour Promise <psaviour495@gmail.com>
2024-06-30 19:55:48 -0700

    myProfile page, myVisit page and Navbar done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 // import Home from "../app/home/home";
 // import Listing from "../app/listing";
-import Reservation from "../app/reservations/reservation";
+// import Reservation from "../app/reservations/reservation";
 // import Signup from "../app/signup/signup";
 // import Login from "../app/login/login";
 // import MyProfile from "../app/myProfile/myProfile";
-import MyVisit from "../app/myVisit/myVisit";
+// import MyVisit from "../app/myVisit/myVisit";
+import Landing_page from "../app/Landing/Landingpage";
 
 export default function Page() {
   return (
@@ -15,6 +16,7 @@ export default function Page() {
       {/* <Signup /> */}
       {/* <Login /> */}
       {/* <MyProfile /> */}
+      <Landing_page />
     </div>
   );
 }
